146048b3ebb2a076312a26faf52b19e9dc91a163
KAYSILVHA <kaysilvha@gmail.com>
2024-11-05 21:17:58 -0300

    "iniciando projeto"

---
+++ src/main/java/com/example/api_user/dto/UserDTO.java
@@ -0,0 +1,43 @@
+package com.example.api_user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private int id;
+    private String email;
+    private String userName;
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
--- src/main/java/com/example/api_user/model/User.java
+++ src/main/java/com/example/api_user/model/User.java
@@ -1,6 +1,8 @@
 package com.example.api_user.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -9,5 +11,19 @@ import lombok.Data;
 @Table(name = "user")
 
 public class User {
+    @Id
+    private int id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private String userName;
 
 }
