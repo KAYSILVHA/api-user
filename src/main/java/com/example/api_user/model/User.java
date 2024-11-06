2f4c3d8087a2ddcb24c6acecc08cd58f1a6bf2f7
KAYSILVHA <kaysilvha@gmail.com>
2024-11-05 21:07:03 -0300

    "iniciando projeto"

---
+++ src/main/java/com/example/api_user/model/User.java
@@ -0,0 +1,13 @@
+package com.example.api_user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user")
+
+public class User {
+
+}
